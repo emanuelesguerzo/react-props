1c7e5f03cda4a8d500da4c7f205d1bca49a0acf0
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-12-13 14:34:20 +0100

    feat: milestone #2 complete, printing only published posts

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,21 +1,28 @@
 import BlogCard from "./BlogCard";
 import posts from "../data/data"
 
+
+
 const AppMain = () => {
     return (
         <main>
             <div className="container">
                 <div className="row">
-                    {posts.map((curPost) => (
-                        <div className="col" key={curPost.id}>
-                            <BlogCard
-                                title={curPost.title}
-                                image={curPost.image || "https://via.placeholder.com/600x400"}
-                                content={curPost.content}
-                                tags={curPost.tags}
-                            />
-                        </div>
-                    ))}                   
+                    {posts.map((curPost) => {
+                        const isPublic = curPost.published;
+                        return (
+                            <div className="col" key={curPost.id}>    
+                                {isPublic && (
+                                    <BlogCard
+                                        title={curPost.title}
+                                        image={curPost.image || "https://via.placeholder.com/600x400"}
+                                        content={curPost.content}
+                                        tags={curPost.tags}
+                                    />
+                                )}  
+                            </div> 
+                        )
+                    })}                 
                 </div>             
             </div>
         </main>
